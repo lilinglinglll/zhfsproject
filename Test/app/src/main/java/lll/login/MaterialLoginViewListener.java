6fea295904dbb673918f22edccd2f8192f441f39
lilinglinglll <369179302@qq.com>
2016-06-14 22:02:59 +0800

    SHANCHU

--- Test/app/src/main/java/lll/login/MaterialLoginViewListener.java
+++
@@ -1,7 +0,0 @@
-package lll.login;
-
-import android.support.design.widget.TextInputLayout;
-
-public interface MaterialLoginViewListener {
-    void onLogin(TextInputLayout loginUser, TextInputLayout loginPass);
-}
